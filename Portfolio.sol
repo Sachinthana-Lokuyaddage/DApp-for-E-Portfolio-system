cdee24f91716707ad992dd3f760f6dfe24d30162
Divani Perera <123438365+ManashviCode@users.noreply.github.com>
2024-10-26 09:17:00 +0530

    Update Portfolio.sol
    updated for experience and social media accounts

--- Portfolio.sol
+++ Portfolio.sol
@@ -1,125 +1,149 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.7.0 <0.9.0;
 
-contract Portfolio{
- 
-  struct Project{
-      uint id;
-      string name;
-      string description;
-      string image;
-      string githubLink;
-  }
+contract Portfolio {
 
-  struct Education{
-      uint id;
-      string date;
-      string degree;
-      string knowledgeAcquired;
-      string instutionName;
-  }
+    struct Project {
+        uint id;
+        string name;
+        string description;
+        string image;
+        string githubLink;
+    }
 
-  struct Experience{
-      uint id;
-      string date;
-      string post;
-      string knowledgeAcquired;
-      string companyName;
-  }
+    struct Education {
+        uint id;
+        string date;
+        string degree;
+        string knowledgeAcquired;
+        string institutionName;
+    }
 
-  struct Hero {
-      uint id;
-      string imageLink;
-      string description;
-      string resumeLink;
-  }
+    struct Experience {
+        uint id;
+        string date;
+        string post;
+        string knowledgeAcquired;
+        string companyName;
+    }
 
-  Project[10] public projects;
-  Education[10] public educationDetails;
-  Experience[10] public experienceDetails;
-  Hero public hero;
+    struct Hero {
+        uint id;
+        string imageLink;
+        string description;
+        string resumeLink;
+        string email; // Add an email field
+    }
 
-  uint projectCount;
-  uint educationCount;
-  uint experienceCount;
-  address public manager;
+    struct SocialMedia {
+        string platform;
+        string link;
+    }
 
-  constructor(){
-      manager = msg.sender;
-      // Initialize hero with default values
-      hero = Hero(0, "QmQ1o1jw4wGtttRcrXbz1xk1QdzLQGZyzULQ79Y6UaojEf", "A motivated software engineer...", "QmT9hstvnvYhYJHWMJkr5p1vdVeApc3o88pYpgTbHjwjZM");
-  }
+    struct Skill {
+        string name;
+        string proficiency; // Example: Beginner, Intermediate, Expert
+    }
 
-  modifier onlyManager(){
-      require(manager == msg.sender, "You are not the manager");
-      _;
-  }
+    Project[] public projects;
+    Education[] public educationDetails;
+    Experience[] public experienceDetails;
+    Hero public hero;
+    SocialMedia[] public socialMediaLinks;
+    Skill[] public skills;
 
-  // Project functions
-  function insertProject(string calldata _name, string calldata _description, string calldata _image, string calldata _githubLink) external {
-      require(projectCount < 10, "Only 10 projects allowed");
-      projects[projectCount] = Project(projectCount, _name, _description, _image, _githubLink);
-      projectCount++;
-  }
+    address public manager;
 
-  function changeProject(string calldata _name, string calldata _description, string calldata _image, string calldata _githubLink, uint _projectCount) external {
-      require(_projectCount >= 0 && _projectCount < 10, "Only 10 projects allowed");
-      projects[_projectCount] = Project(_projectCount, _name, _description, _image, _githubLink);
-  }
+    constructor() {
+        manager = msg.sender;
+        // Initialize hero with default values, including email
+        hero = Hero(0, "QmQ1o1jw4wGtttRcrXbz1xk1QdzLQGZyzULQ79Y6UaojEf", "A motivated software engineer.....", "QmT9hstvnvYhYJHWMJkr5p1vdVeApc3o88pYpgTbHjwjZM", "sachinthanabuddhika79@gmail.com");
+    }
 
-  function allProjects() external view returns(Project[10] memory){
-      return projects;
-  }
+    modifier onlyManager() {
+        require(manager == msg.sender, "You are not the manager");
+        _;
+    }
 
-  // Education functions
-  function insertEducation(string calldata _date, string calldata _degree, string calldata _knowledgeAcquired, string calldata _instutionName) external onlyManager {
-      require(educationCount < 10, "Only 10 education details allowed");
-      educationDetails[educationCount] = Education(educationCount, _date, _degree, _knowledgeAcquired, _instutionName);
-      educationCount++;
-  }
+    // Project functions
+    function insertProject(string calldata _name, string calldata _description, string calldata _image, string calldata _githubLink) external {
+        projects.push(Project(projects.length, _name, _description, _image, _githubLink));
+    }
 
-  function changeEducation(string calldata _date, string calldata _degree, string calldata _knowledgeAcquired, string calldata _instutionName, uint _educationDetailCount) external onlyManager {
-      require(_educationDetailCount >= 0 && _educationDetailCount < 10, "Invalid educationCount");
-      educationDetails[_educationDetailCount] = Education(_educationDetailCount, _date, _degree, _knowledgeAcquired, _instutionName);
-  }
+    function changeProject(string calldata _name, string calldata _description, string calldata _image, string calldata _githubLink, uint _projectId) external {
+        require(_projectId < projects.length, "Invalid project ID");
+        projects[_projectId] = Project(_projectId, _name, _description, _image, _githubLink);
+    }
 
-  function allEductationDetails() external view returns(Education[10] memory){
-      return educationDetails;
-  }
+    function allProjects() external view returns(Project[] memory) {
+        return projects;
+    }
 
-  // Experience functions
-  function insertExperience(string calldata _date, string calldata _post, string calldata _knowledgeAcquired, string calldata _companyName) external onlyManager {
-      require(experienceCount < 10, "Only 10 experience details allowed");
-      experienceDetails[experienceCount] = Experience(experienceCount, _date, _post, _knowledgeAcquired, _companyName);
-      experienceCount++;
-  }
+    // Education functions
+    function insertEducation(string calldata _date, string calldata _degree, string calldata _knowledgeAcquired, string calldata _institutionName) external onlyManager {
+        educationDetails.push(Education(educationDetails.length, _date, _degree, _knowledgeAcquired, _institutionName));
+    }
 
-  function changeExperience(string calldata _date, string calldata _post, string calldata _knowledgeAcquired, string calldata _companyName, uint _experienceDetailCount) external onlyManager {
-      require(_experienceDetailCount >= 0 && _experienceDetailCount < 10, "Invalid experienceCount");
-      experienceDetails[_experienceDetailCount] = Experience(_experienceDetailCount, _date, _post, _knowledgeAcquired, _companyName);
-  }
+    function changeEducation(string calldata _date, string calldata _degree, string calldata _knowledgeAcquired, string calldata _institutionName, uint _educationId) external onlyManager {
+        require(_educationId < educationDetails.length, "Invalid education ID");
+        educationDetails[_educationId] = Education(_educationId, _date, _degree, _knowledgeAcquired, _institutionName);
+    }
 
-  function allExperienceDetails() external view returns(Experience[10] memory){
-      return experienceDetails;
-  }
+    function allEducationDetails() external view returns(Education[] memory) {
+        return educationDetails;
+    }
 
-  // Hero functions
-  function insertHero(string calldata _imageLink, string calldata _description, string calldata _resumeLink) external onlyManager {
-      hero = Hero(0, _imageLink, _description, _resumeLink);
-  }
+    // Experience functions
+    function insertExperience(string calldata _date, string calldata _post, string calldata _knowledgeAcquired, string calldata _companyName) external onlyManager {
+        experienceDetails.push(Experience(experienceDetails.length, _date, _post, _knowledgeAcquired, _companyName));
+    }
 
-  function changeHero(string calldata _imageLink, string calldata _description, string calldata _resumeLink) external onlyManager {
-      hero = Hero(0, _imageLink, _description, _resumeLink);
-  }
+    function changeExperience(string calldata _date, string calldata _post, string calldata _knowledgeAcquired, string calldata _companyName, uint _experienceId) external onlyManager {
+        require(_experienceId < experienceDetails.length, "Invalid experience ID");
+        experienceDetails[_experienceId] = Experience(_experienceId, _date, _post, _knowledgeAcquired, _companyName);
+    }
 
-  function getHero() external view returns(Hero memory) {
-      return hero;
-  }
+    function allExperienceDetails() external view returns(Experience[] memory) {
+        return experienceDetails;
+    }
 
- 
+    // Hero functions
+    function insertHero(string calldata _imageLink, string calldata _description, string calldata _resumeLink, string calldata _email) external onlyManager {
+        hero = Hero(0, _imageLink, _description, _resumeLink, _email);
+    }
 
-  // Payment functionality
-  function donate() public payable {
-      payable(manager).transfer(msg.value);
-  }
+    function changeHero(string calldata _imageLink, string calldata _description, string calldata _resumeLink, string calldata _email) external onlyManager {
+        hero = Hero(0, _imageLink, _description, _resumeLink, _email);
+    }
+
+    function getHero() external view returns (Hero memory) {
+        return hero;
+    }
+
+    function getEmail() external view returns (string memory) {
+        return hero.email;
+    }
+
+    // Social Media functions
+    function addSocialMedia(string calldata _platform, string calldata _link) external onlyManager {
+        socialMediaLinks.push(SocialMedia(_platform, _link));
+    }
+
+    function getSocialMedia() external view returns(SocialMedia[] memory) {
+        return socialMediaLinks;
+    }
+
+    // Skill functions
+    function addSkill(string calldata _name, string calldata _proficiency) external onlyManager {
+        skills.push(Skill(_name, _proficiency));
+    }
+
+    function getSkills() external view returns(Skill[] memory) {
+        return skills;
+    }
+
+    // Payment functionality
+    function donate() public payable {
+        payable(manager).transfer(msg.value);
+    }
 }
